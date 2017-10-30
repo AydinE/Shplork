11bafd803cc7b404a8847a73f9c2a4820482a91a
Aydin Erdas <erdas001@gmail.com>
2017-10-30 10:48:24 +0100

    Changes to program

--- ConsoleAppjeMaandag/Program.cs
+++ ConsoleAppjeMaandag/Program.cs
@@ -7,6 +7,19 @@ namespace ConsoleAppjeMaandag
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            PrintMethod();            
+
+            String result = Console.ReadLine();
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+        public static void PrintMethod()
+        {
+            Console.WriteLine("Hai");
         }
     }
 }
