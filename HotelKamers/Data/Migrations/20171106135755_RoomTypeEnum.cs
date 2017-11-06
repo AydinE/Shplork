16a67dead9b56b5478153fcc9234d5baf27feb9e
Aydin Erdas <erdas001@gmail.com>
2017-11-06 16:13:28 +0100

    Webappje

---
+++ HotelKamers/Data/Migrations/20171106135755_RoomTypeEnum.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HotelKamers.Data.Migrations
+{
+    public partial class RoomTypeEnum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE dbo.Room SET Type = 0");
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Room",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Room",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
---
+++ HotelKamers/Models/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelKamers.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        [Display(Name = "Available from")]
+        [DataType(DataType.Date)]
+        public DateTime AvailableFrom { get; set; }
+        public RoomType Type { get; set; }
+    }
+}
